55ab468a4cb413511a7ce8bb270371d9d3bc9bb0
Caio Graça Melo <caio.g.melo@hotmail.com>
2025-08-05 18:50:47 -0300

    Add a league column to the NowGoal data table

--- src/get_nowgoal_data.py
+++ src/get_nowgoal_data.py
@@ -1,11 +1,15 @@
+import typing as t
 from services.mysql import MySQLService
 from services.scrapper.nowgoal import NowGoalScrapperService
 from dotenv import load_dotenv
 import pandas as pd
 
+from utils.helper_functions import get_league_str
 
-def create_mysql_nowgoal_matches_table(matches_data_df: pd.DataFrame):
-    pk_cols = ["season", "round", "home_team", "away_team"]
+
+def create_mysql_nowgoal_matches_table(
+    matches_data_df: pd.DataFrame, pk_cols: t.List[str]
+) -> None:
     mysql_service = MySQLService()
 
     mysql_service.create_table_from_df("nowgoal_matches", matches_data_df, pk_cols)
@@ -13,12 +17,12 @@ def create_mysql_nowgoal_matches_table(matches_data_df: pd.DataFrame):
     mysql_service.close()
 
 
-def insert_nowgoal_matches(matches_data_df: pd.DataFrame):
+def insert_nowgoal_matches(matches_data_df: pd.DataFrame, pk_cols: t.List[str]) -> None:
     mysql_service = MySQLService()
 
     data_list = matches_data_df.to_dict(orient="records")
 
-    mysql_service.insert_multiple_rows("nowgoal_matches", data_list)
+    mysql_service.insert_multiple_rows("nowgoal_matches", data_list, pk_cols)
 
     mysql_service.close()
 
@@ -30,6 +34,8 @@ first_season = 2012
 last_season = 2025
 
 league_id = 36
+league = "premier-league"
+country = "england"
 
 create_nowgoal_matches_table = True
 single_year_season = False
@@ -50,9 +56,13 @@ for season in range(first_season, last_season):
 
     scrapper.nowgoal_scrapper()
 
-    if create_nowgoal_matches_table and season == first_season:
-        create_mysql_nowgoal_matches_table(scrapper.nowgoal_data_df)
+    pk_cols = ["date", "home_team", "away_team"]
 
-    insert_nowgoal_matches(scrapper.nowgoal_data_df)
+    scrapper.nowgoal_data_df["league"] = get_league_str(country, league)
+
+    if create_nowgoal_matches_table and season == first_season:
+        create_mysql_nowgoal_matches_table(scrapper.nowgoal_data_df, pk_cols)
+
+    insert_nowgoal_matches(scrapper.nowgoal_data_df, pk_cols)
 
     scrapper.close_driver()
